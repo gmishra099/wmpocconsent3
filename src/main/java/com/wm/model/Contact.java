bdada17993232d6b0dce641749c054b26f405f9c
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-21 20:35:57 +0530

    Update Contact.java

--- src/main/java/com/wm/model/Contact.java
+++ src/main/java/com/wm/model/Contact.java
@@ -1,8 +1,10 @@
 package com.wm.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "contact", schema="salesforce")
