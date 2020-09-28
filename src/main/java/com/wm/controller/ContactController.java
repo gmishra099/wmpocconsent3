70f93a232c224edb9edeea2a808b31e3c4a96492
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-28 22:40:02 +0530

    Update ContactController.java

--- src/main/java/com/wm/controller/ContactController.java
+++ src/main/java/com/wm/controller/ContactController.java
@@ -96,7 +96,7 @@ public class ContactController {
 
 	
 			@PutMapping("/test/{id}")
-		public Consent updateEmployee(@PathVariable(value = "id") String custId,
+		public Individual updateEmployee(@PathVariable(value = "id") String custId,
 				@Validated @RequestBody Individual indDetail){
 			Contact con=contactService.findByCustId(custId);
 			String indSFID= con.getIndividualid();
@@ -106,17 +106,17 @@ public class ContactController {
 			ind.setShouldforget(indDetail.getShouldforget());
 			individualService.save(ind);
 			List<Consent> consents=consentService.findByconsentGiverId(indSFID);
-			Consent con1 = null;
+			
 			for(Consent consent: consents) {
 	
-				con1 =consentService.findByComm_sub__cAndcontactpointid("News Updates", "0Ow2w000000XZAMCA4");
+				Consent con1 =consentService.findByComm_sub__cAndcontactpointid("News Updates", "0Ow2w000000XZAMCA4");
 				//con1=consentService.findByComm_sub__cAndcontactpointid(consent.getComm_sub__c(), consent.getContactPointId());
 				con1.setConsent__c("Opt-In");
 				System.out.println("consent test value"+con1.getConsent__c());
 				consentService.save(con1);
 				
 			}
-			return con1;
+			return ind;
 		}
 	
 	
