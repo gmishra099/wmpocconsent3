65123146e9a7b22218501cc61d2d9ae1e92e3302
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-28 21:36:13 +0530

    Update ConsentRepository.java

--- src/main/java/com/wm/repository/ConsentRepository.java
+++ src/main/java/com/wm/repository/ConsentRepository.java
@@ -2,10 +2,13 @@ package com.wm.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wm.model.Consent;
 @Repository
 public interface ConsentRepository extends JpaRepository<Consent, Long> {
 	List<Consent> findByconsentGiverId(String consentGiverId);
+	@Query( value = "SELECT * FROM commsubscriptionconsent u WHERE Comm_sub__c = ?1 AND contactpointid =?2",nativeQuery = true)
+	Consent findByComm_sub__cAndcontactpointid(String getComm_sub__c, String getContactPointId);
 }
