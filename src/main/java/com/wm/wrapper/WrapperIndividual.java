112aa847305acbef4215c68e74a70822ee9c71ad
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-28 13:53:41 +0530

    Update WrapperIndividual.java

--- src/main/java/com/wm/wrapper/WrapperIndividual.java
+++ src/main/java/com/wm/wrapper/WrapperIndividual.java
@@ -5,41 +5,51 @@ import java.util.List;
 public class WrapperIndividual {
 	private String cusId;
 	private String name;
-	private String Hasoptedouttracking;
-	private String Shouldforget;
+	private Boolean Hasoptedouttracking;
+	private Boolean Shouldforget;
 	
 	List<WrapperConsent> WrapperConsentList;
-	
-	
-	public List<WrapperConsent> getWrapperConsentList() {
-		return WrapperConsentList;
-	}
-	public void setWrapperConsentList(List<WrapperConsent> wrapperConsentList) {
-		WrapperConsentList = wrapperConsentList;
-	}
+
 	public String getCusId() {
 		return cusId;
 	}
+
 	public void setCusId(String cusId) {
 		this.cusId = cusId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getHasoptedouttracking() {
+
+	public Boolean getHasoptedouttracking() {
 		return Hasoptedouttracking;
 	}
-	public void setHasoptedouttracking(String hasoptedouttracking) {
+
+	public void setHasoptedouttracking(Boolean hasoptedouttracking) {
 		Hasoptedouttracking = hasoptedouttracking;
 	}
-	public String getShouldforget() {
+
+	public Boolean getShouldforget() {
 		return Shouldforget;
 	}
-	public void setShouldforget(String shouldforget) {
+
+	public void setShouldforget(Boolean shouldforget) {
 		Shouldforget = shouldforget;
 	}
+
+	public List<WrapperConsent> getWrapperConsentList() {
+		return WrapperConsentList;
+	}
+
+	public void setWrapperConsentList(List<WrapperConsent> wrapperConsentList) {
+		WrapperConsentList = wrapperConsentList;
+	}
+	
+
 	
 }
