4c6fff653d72a9b9603832cea5fed867c9664403
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-28 21:35:08 +0530

    Update ConsentService.java

--- src/main/java/com/wm/service/ConsentService.java
+++ src/main/java/com/wm/service/ConsentService.java
@@ -7,4 +7,5 @@ import com.wm.model.Contact;
 
 public interface ConsentService {
 	public List<Consent> findByconsentGiverId(String consentGiverId);
+	public Consent findByComm_sub__cAndcontactpointid(String getComm_sub__c, String coontactPointId);
 }
