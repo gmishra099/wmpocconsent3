cb344889227c0186082a3916fe2b5756a6876074
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-29 11:54:19 +0530

    Update ContactPointPhoneService.java

--- src/main/java/com/wm/service/ContactPointPhoneService.java
+++ src/main/java/com/wm/service/ContactPointPhoneService.java
@@ -4,4 +4,5 @@ import com.wm.model.ContactPointPhone;
 
 public interface ContactPointPhoneService {
 	public ContactPointPhone findBycontactPointPhonesfid(String contactPointId);
+	public ContactPointPhone findBytelephoneNumber(String telephoneNumber);
 }
