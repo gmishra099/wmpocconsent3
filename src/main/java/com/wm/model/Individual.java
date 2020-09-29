5c4d7101629391a6313c756bc37387206558c0f5
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-29 15:33:03 +0530

    Update Individual.java

--- src/main/java/com/wm/model/Individual.java
+++ src/main/java/com/wm/model/Individual.java
@@ -16,54 +16,79 @@ public class Individual {
     @Column(name = "Name")
     private String name;
     
+    @Column(name = "Lastname")
+    private String lastName;
+
+    
     @Column(name = "sfid")
     private String indSFID;
     
     @Column(name = "Hasoptedouttracking")
     private Boolean Hasoptedouttracking;
     
-    public Long getId() {
+
+	@Column(name = "Shouldforget")
+    private Boolean Shouldforget;
+
+
+	public Long getId() {
 		return id;
 	}
+
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+
 	public String getName() {
 		return name;
 	}
+
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
 	public String getIndSFID() {
 		return indSFID;
 	}
+
 
 	public void setIndSFID(String indSFID) {
 		this.indSFID = indSFID;
 	}
 
+
 	public Boolean getHasoptedouttracking() {
 		return Hasoptedouttracking;
 	}
+
 
 	public void setHasoptedouttracking(Boolean hasoptedouttracking) {
 		Hasoptedouttracking = hasoptedouttracking;
 	}
 
+
 	public Boolean getShouldforget() {
 		return Shouldforget;
 	}
 
+
 	public void setShouldforget(Boolean shouldforget) {
 		Shouldforget = shouldforget;
 	}
-
-	@Column(name = "Shouldforget")
-    private Boolean Shouldforget;
 
 	
     
