5f6fbd81c25f343aaba4f114d7b4811560208804
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-29 15:32:29 +0530

    Update WrapperIndividual.java

--- src/main/java/com/wm/wrapper/WrapperIndividual.java
+++ src/main/java/com/wm/wrapper/WrapperIndividual.java
@@ -1,55 +1,96 @@
-package com.wm.wrapper;
+package com.wm.model;
 
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-public class WrapperIndividual {
-	private String cusId;
-	private String name;
-	private Boolean Hasoptedouttracking;
-	private Boolean Shouldforget;
-	
-	List<WrapperConsent> WrapperConsentList;
+@Entity
+@Table(name = "individual", schema="salesforce")
+public class Individual {
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column(name = "Name")
+    private String name;
+    
+    @Column(name = "Lastname")
+    private String lastName;
 
-	public String getCusId() {
-		return cusId;
+    
+    @Column(name = "sfid")
+    private String indSFID;
+    
+    @Column(name = "Hasoptedouttracking")
+    private Boolean Hasoptedouttracking;
+    
+
+	@Column(name = "Shouldforget")
+    private Boolean Shouldforget;
+
+
+	public Long getId() {
+		return id;
 	}
 
-	public void setCusId(String cusId) {
-		this.cusId = cusId;
+
+	public void setId(Long id) {
+		this.id = id;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getIndSFID() {
+		return indSFID;
+	}
+
+
+	public void setIndSFID(String indSFID) {
+		this.indSFID = indSFID;
+	}
+
 
 	public Boolean getHasoptedouttracking() {
 		return Hasoptedouttracking;
 	}
 
+
 	public void setHasoptedouttracking(Boolean hasoptedouttracking) {
 		Hasoptedouttracking = hasoptedouttracking;
 	}
+
 
 	public Boolean getShouldforget() {
 		return Shouldforget;
 	}
 
+
 	public void setShouldforget(Boolean shouldforget) {
 		Shouldforget = shouldforget;
 	}
 
-	public List<WrapperConsent> getWrapperConsentList() {
-		return WrapperConsentList;
-	}
-
-	public void setWrapperConsentList(List<WrapperConsent> wrapperConsentList) {
-		WrapperConsentList = wrapperConsentList;
-	}
 	
+    
 
-	
 }
