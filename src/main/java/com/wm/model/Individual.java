6f8c050f1f5838dae16b5931a5a4e9750fbbeb9b
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-29 15:44:12 +0530

    Update Individual.java

--- src/main/java/com/wm/model/Individual.java
+++ src/main/java/com/wm/model/Individual.java
@@ -5,12 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name = "individual", schema="salesforce")
 public class Individual {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Column(name = "Name")
