f662b2e25de6c301ac1120dacb606aba2c1bb30a
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-29 15:31:41 +0530

    Update ContactController.java

--- src/main/java/com/wm/controller/ContactController.java
+++ src/main/java/com/wm/controller/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
